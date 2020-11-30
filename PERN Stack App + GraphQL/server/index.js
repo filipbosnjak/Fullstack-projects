17c1ebefd98312913b54a839c1b1e8b07e1e2b5a
filipbosnjak <filipo.bosnjak@gmail.com>
2020-11-30 20:48:35 +0100

    GraphQL App

---
+++ PERN Stack App + GraphQL/server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql"); //Binds express and graphql
+
+const app = express();
+app.use("/graphql", graphqlHTTP({}));
+
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Hello world!",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("server is running...");
+});
